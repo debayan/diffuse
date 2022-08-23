2919d35cbef3083abd5932cba8cc65f49ccd5d06
Debayan Banerjee <debayanin@gmail.com>
2022-08-23 10:52:41 +0200

    initial commit

---
+++ diffuse.py
@@ -0,0 +1,32 @@
+from torch import autocast
+from flask import request,jsonify,Flask
+from diffusers import StableDiffusionPipeline, LMSDiscreteScheduler
+
+app = Flask(__name__)
+
+lms = LMSDiscreteScheduler(
+            beta_start=0.00085, 
+                beta_end=0.012, 
+                    beta_schedule="scaled_linear"
+                    )
+
+pipe = StableDiffusionPipeline.from_pretrained(
+            "CompVis/stable-diffusion-v1-4", 
+                scheduler=lms,
+                    use_auth_token="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"
+                    ).to("cuda")
+
+@app.route("/diffuse",methods=['POST'])
+def diffuse():
+    user_data = request.get_json()
+    prompt = user_data['prompt']
+    with autocast("cuda"):
+        image = pipe(prompt)["sample"][0]  
+    print(prompt)
+    prompt = prompt.replace(' ','_')
+    fname = "images/%s.png"%(prompt) 
+    image.save(fname)
+    return(jsonify({'status':'ok'}))
+
+
+
